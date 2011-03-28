f9f7d65da81010bcd8aca05a2f3c08fa201efbf6
Vladymir Bezerra <vlad.bezerra@gmail.com>
2011-03-28 01:24:12 -0300

    primeiro commit, ambiente instalado.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+		OclCup cup = new OclCup(new OCL_Lexer(new FileReader("/home/vladymir/teste")));
+		try {cup.parse(); 
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("vlad");
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
